c48f1d264e8fe7bde717c5747cb075efbc443977
Dmitrienko Artem <admitienko@mail.ru>
2023-08-03 15:17:18 +0300

    work_with_database done

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.TextField(max_length=30)
+    slug = models.SlugField(null=True)
+
+    price = models.IntegerField()
+    image = models.ImageField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"slug": self.name})
+
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,16 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
 
+    phones = Phone.objects.all()
+    context = {'phones': phones}
+    return render(request, template, context)
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug)
+    for c in phone:
+        result_phone = {'name': c.name, 'image': c.image, 'price': c.price, 'release_date': c.release_date, 'lte_exist': c.lte_exists }
+
+    context = {'phone': result_phone}
     return render(request, template, context)
