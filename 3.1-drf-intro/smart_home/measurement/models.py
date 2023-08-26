57988338fc9da8c320cb7716c9e72e7b7b3f83e9
Dmitrienko Artem <admitienko@mail.ru>
2023-08-26 23:16:25 +0300

    Initial commit

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=120)
+
+class Measurement(models.Model):
+    temperature = models.FloatField()
+    created_at = models.DateField(auto_now_add=True)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,48 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework import status
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from measurement.models import Sensor, Measurement
+from measurement.serializers import SensorSerializer, MeasurementSerializer
+
+
+class SensorView(RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    def patch(self, request, pk):
+        testmodel_object = Sensor.objects.get(pk=pk)
+        serializer = SensorSerializer(testmodel_object, data=request.data,
+                                         partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SensorsView(ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    def post(self, request):
+        serializer = SensorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MeasurementsView(APIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+    def post(self, request):
+        serializer = MeasurementSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
